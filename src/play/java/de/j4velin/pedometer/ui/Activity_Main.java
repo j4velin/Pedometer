f9d12ed302afe22036262b7ab287673888662b2c
musaturaptolay1 <turap.tolay@bil.omu.edu.tr>
2019-05-17 11:09:28 +0300

    location
    I've added a code snippet that finds the difference between the start and end position on google maps, which finds the user's location for this project. This way, the user will be able to see how much he has traveled.

--- src/play/java/de/j4velin/pedometer/ui/Activity_Main.java
+++ src/play/java/de/j4velin/pedometer/ui/Activity_Main.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
 
 package de.j4velin.pedometer.ui;
 
@@ -272,4 +274,69 @@ public class Activity_Main extends FragmentActivity implements GoogleApiClient.C
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+}
+package com.tag.gpsex;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends Activity {
+  private WebView position;
+  private TextView textInfo;
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    position = (WebView) findViewById(R.id.webView);
+    position.getSettings().setJavaScriptEnabled(true);
+    textInfo = (TextView) findViewById(R.id.textInfo);
+    textInfo.setText("Location : ");
+    LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    //For Retrieving new Location
+    LocationListener locListener = new MyLocationListener();
+    locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+        locListener);
+  }
+  public class MyLocationListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+      
+      // Retrieving Latitude
+      location.getLatitude();
+      // Retrieving getLongitude
+      location.getLongitude();
+      
+      textInfo.setText("");
+      String text = "My Current Location is:\nLatitude = "
+          + location.getLatitude() + "\nLongitude = "
+          + location.getLongitude();
+      textInfo.setText(text);
+      Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT)
+          .show();
+      // set Google Map on webview
+      String url = "http://maps.google.com/staticmap?center="
+          + location.getLatitude() + "," + location.getLongitude()
+          + "&zoom=14&size=512x512&maptype=mobile/&markers="
+          + location.getLatitude() + "," + location.getLongitude();
+      position.loadUrl(url);
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+      Toast.makeText(getApplicationContext(), "GPS Disabled",
+          Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onProviderEnabled(String provider) {
+      Toast.makeText(getApplicationContext(), "GPS Enabled",
+          Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+  }
 }
